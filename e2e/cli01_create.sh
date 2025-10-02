3882317895fc43598d19c83258ed6fd1eea49b42
Alex Kraasch <alex@kraasch.eu>
2025-10-02 14:04:52 +0200

    Setup test dirs.

---
+++ e2e/cli01_create.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+echo "Creation script for ./cli01/ at '$(pwd)'."
+
+# clean.
+rm -rf ./cli01
+
+# create directories and files.
+mkdir ./cli01
+mkdir ./cli01/cfg
+mkdir ./cli01/targetDir0
+mkdir ./cli01/targetDir0/firefox
+mkdir ./cli01/targetDir0/downloads
+mkdir ./cli01/targetDir0/downloads/done
+mkdir ./cli01/targetDir0/downloads/incomplete
+echo 'ABC content' >>./cli01/targetDir0/abc.txt
+echo 'ABC content' >>./cli01/targetDir0/firefox/abc.txt
+echo 'ABC content' >>./cli01/targetDir0/downloads/abc.txt
+echo 'ABC content' >>./cli01/targetDir0/downloads/done/abc.txt
+echo 'ABC content' >>./cli01/targetDir0/downloads/incomplete/abc.txt
+
+# copy directories.
+cp -r ./cli01/targetDir0 ./cli01/targetDir1
+
+# insert files.
+cp ./cli01_config.toml cli01/cfg/config.toml
+cp ./cli01_watchcat.txt cli01/targetDir1/.Watchcat
